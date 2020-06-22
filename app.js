5d1749b129fdc341077037d49e38b86c99ccb198
warecuber <warecuber@gmail.com>
2020-06-21 20:21:53 -0500

    Added timer

--- app.js
+++ app.js
@@ -69,8 +69,50 @@ var scrambleController = (() => {
 ///////////////////////////////////////
 
 var timer = (() => {
-  let running = false;
-  var color = "white";
+  var ms = 0,
+    s = 0,
+    m = 0,
+    running = false,
+    color = "white";
+  var timer;
+  var stopwatch = document.querySelector(".time");
+
+  function start() {
+    if (!timer) {
+      ms = 0;
+      s = 0;
+      m = 0;
+      timer = setInterval(run, 10);
+    }
+  }
+
+  function run() {
+    if (m <= 0) {
+      stopwatch.textContent = `${s < 10 ? `0${s}` : s}:${
+        ms < 10 ? `0${s}` : ms
+      }`;
+    } else {
+      stopwatch.textContent = `${m}:${s}:${ms}`;
+    }
+
+    ms++;
+    if (ms == 100) {
+      ms = 0;
+      s++;
+    }
+    if (s == 60) {
+      s = 0;
+      m++;
+    }
+  }
+
+  function stop() {
+    clearInterval(timer);
+    timer = false;
+    scrambleController.scramble();
+  }
+
+  function recordTime() {}
 
   function init() {
     window.addEventListener("keyup", (e) => {
@@ -82,18 +124,7 @@ var timer = (() => {
           $(".time").css({
             color: "rgb(214, 214, 214)",
           });
-          var start = new Date().getTime(),
-            elapsed = "0.0";
-          if (running) {
-            window.setInterval(function () {
-              var time = new Date().getTime() - start;
-              elapsed = Math.floor(time / 10) / 100;
-              if (Math.round(elapsed) == elapsed) {
-                elapsed += ".0";
-              }
-              document.querySelector(".time").innerHTML = elapsed;
-            }, 10);
-          }
+          start();
         } else if (color !== "green") {
           $(".time").css({
             color: "rgb(214, 214, 214)",
@@ -104,13 +135,11 @@ var timer = (() => {
     window.addEventListener("keydown", (e) => {
       if (running) {
         console.log("stopped");
-        setTimeout(() => {
-          running = false;
-        }, 500);
+        stop();
+        running = false;
       } else {
         if (e.keyCode === 32) {
           console.log(color);
-
           if (color === "white") {
             $(".time").css({
               color: "rgb(252, 164, 0)",
@@ -130,6 +159,68 @@ var timer = (() => {
     });
     scrambleController.scramble();
   }
+
+  // let running = false;
+  // var color = "white";
+
+  // function init() {
+  //   window.addEventListener("keyup", (e) => {
+  //     if (e.keyCode === 32) {
+  //       if (!running && color === "green") {
+  //         color = "white";
+  //         console.log("started");
+  //         running = true;
+  //         $(".time").css({
+  //           color: "rgb(214, 214, 214)",
+  //         });
+  //         var start = new Date().getTime(),
+  //           elapsed = "0.0";
+  //         if (running) {
+  //           window.setInterval(function () {
+  //             var time = new Date().getTime() - start;
+  //             elapsed = Math.floor(time / 10) / 100;
+  //             if (Math.round(elapsed) == elapsed) {
+  //               elapsed += ".0";
+  //             }
+  //             document.querySelector(".time").innerHTML = elapsed;
+  //           }, 10);
+  //         }
+  //       } else if (color !== "green") {
+  //         $(".time").css({
+  //           color: "rgb(214, 214, 214)",
+  //         });
+  //       }
+  //     }
+  //   });
+  //   window.addEventListener("keydown", (e) => {
+  //     if (running) {
+  //       console.log("stopped");
+  //       setTimeout(() => {
+  //         running = false;
+  //       }, 500);
+  //     } else {
+  //       if (e.keyCode === 32) {
+  //         console.log(color);
+
+  //         if (color === "white") {
+  //           $(".time").css({
+  //             color: "rgb(252, 164, 0)",
+  //           });
+  //           color = "orange";
+  //         }
+  //         setTimeout(() => {
+  //           if (color === "orange") {
+  //             $(".time").css({
+  //               color: "rgb(8, 187, 8)",
+  //             });
+  //             color = "green";
+  //           }
+  //         }, 1000);
+  //       }
+  //     }
+  //   });
+  //   scrambleController.scramble();
+  // }
 
   return {
     init: () => {
